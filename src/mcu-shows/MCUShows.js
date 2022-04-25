5c891d7761ccf5929bcf2c9801589c22257da07a
DwightDubert <dwight.dubert@stu.caverna.kyschools.us>
2022-04-25 10:03:45 -0500

    styling-state-props-exercise

---
+++ src/mcu-shows/MCUShows.js
@@ -0,0 +1,19 @@
+import './MCUShows.css';
+
+function MCUShows({dates}){
+  return(
+    <div className='MCUShows'>
+      <h1 style={{fontFamily: 'Georgia', textDecoration: 'underline'}}>Disney+</h1>
+      <h2>MCU Show Release Dates</h2>
+      <p>WandaVision: {dates.wandaVision}</p>
+      <p>The Falcon and the Winter Soldier: {dates.falconWinter}</p>
+      <p>Loki: {dates.loki}</p>
+      <p>What If...?: {dates.whatIf}</p>
+      <p>Hawkeye: {dates.hawkeye}</p>
+      <p>Moon Knight: {dates.moonKnight}</p>
+      <p>Ms. Marvel: {dates.msMarvel}</p>
+    </div>
+  );
+}
+
+export default MCUShows;
